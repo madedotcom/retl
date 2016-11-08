b9f275479df0fdb143c631343d5d07fcdaf6dbbe
byapparov <byapparov@gmail.com>
2016-11-08 10:39:36 +0000

    Added test and updated the packrat.lock file.

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(retl)
+
+test_check("retl")
---
+++ tests/testthat/test.bq.date.R
@@ -0,0 +1,12 @@
+context("BigQuery Dates Tables/Partitions.")
+
+test_that("Missing dates function is correct.", {
+  start.date <- as.Date("2015-01-10")
+  end.date <- as.Date("2015-01-20")
+  # Dates are given in format %Y%m%d.
+  existing.dates <-c("20150110", "20150111", "20150112")
+  res <- getMissingDates(start.date, end.date, existing.dates)
+
+  expect_identical(length(res), as.integer(8), "Existing dates are exluded.")
+
+})
