adabbdb4a825f33b14c9eab0a742da0ef3c675f2
Bulat Yapparov <byapparov@gmail.com>
2017-04-07 15:17:21 +0100

    Exposed access_token file as parameter.

--- R/googlespreadsheets.R
+++ R/googlespreadsheets.R
@@ -7,9 +7,10 @@ library(data.table)
 #' Loads google spreadsheet via key
 #'
 #' @param key key of the google spreadsheet
-gsLoadSheet <- function(key, tab) {
-  service_token <- gar_auth_service(json_file="access_token.json", scope = c("https://www.googleapis.com/auth/drive",
-                                                                             "https://spreadsheets.google.com/feeds"))
+#' @param token.file json access token file for Google API.
+gsLoadSheet <- function(key, tab, token.file = "access_token.json") {
+  service_token <- gar_auth_service(json_file = token.file, scope = c("https://www.googleapis.com/auth/drive",
+                                                                      "https://spreadsheets.google.com/feeds"))
   gs_auth(token = service_token)
   gap <- gs_key(key, verbose = T)
   res <- gs_read(ss = gap, ws = tab)
