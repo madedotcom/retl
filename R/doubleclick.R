45c715198dc4c2858b7af690814ced81d1d97dfe
Bulat Yapparov <byapparov@gmail.com>
2017-04-12 14:35:38 +0100

    Added function that writes transactions to DC

--- R/doubleclick.R
+++ R/doubleclick.R
@@ -4,6 +4,23 @@
 library(jsonlite)
 library(googleAuthR)
 
+# Authentication requires following environment variables:
+# GOOGLE_CLIENT_ID ?
+# GOOGLE_CLIENT_SECRET ?
+# GOOGLE_REFRESH_TOKEN
+
+# Scope required for authentication:
+# https://www.googleapis.com/auth/doubleclicksearch
+
+# Documentation on getting the refresh_token:
+# https://developers.google.com/doubleclick-search/v2/prereqs#ds3py
+
+# Set options for Google DC API, possibly should be in .onLoad file.
+options(googleAuthR.client_id = Sys.getenv("GOOGLE_CLIENT_ID"),
+        googleAuthR.client_secret = Sys.getenv("GOOGLE_CLIENT_SECRET"),
+        googleAuthR.scopes.selected = c("https://www.googleapis.com/auth/doubleclicksearch"),
+        googleAuthR.httr_oauth_cache = TRUE)
+
 
 #' Creates list for the DoubleClick API call from prediction variables
 #'
@@ -41,15 +58,20 @@ metricsToList <- function(metrics) {
 }
 
 
-dcWriteConversion <-gar_api_generator("https://www.googleapis.com/doubleclicksearch/v2/conversion", http_header = "POST")
+dcGetApiUri <- function() {
+  uri <- paste0("https://www.googleapis.com/doubleclicksearch/v2/conversion?key=", Sys.getenv("GOOGLE_REFRESH_TOKEN"))
+}
 
+dcWriteConversion <-gar_api_generator(dcGetApiUri(), http_header = "POST")
 
+#' Writes custom metrics to DoubleClick Floodlight
+#' requires gar_auth()
+#'
+#' @param clickId gclid unique identifier of the google click
+#' @param conversionId unique identifier of the conversion
+#' @param metrics named vector of custom metrics
 dcWriteCustomMetics <- function(clickId, conversionId, metrics) {
-
   ts <- paste0(as.integer(as.POSIXct( Sys.time() )) * 1000)
   body <- dcPredictionBody(clickId, conversionId, datetime = ts, metrics)
-  print(body)
-  gar_auth_service(json_file = "access_token.json")
   res <- dcWriteConversion(the_body = body)
-  print(res)
 }
---
+++ R/tests.R
@@ -0,0 +1,4 @@
+
+# dcWriteCustomMetics("CJPmyaSYmdMCFVcW0wodHMAE2w", "23436345J", c("Predicted Revenue" = 10, "Predicted Transactions" = 0.1))
+
+
