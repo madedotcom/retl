de09fc59fe6487c6341e01290b5933a1ef2918e4
daniel0000 <daniel.ferenczy@made.com>
2017-06-29 11:25:50 +0100

    gsLoadAll: new function for downloading all sheets from a workbook.

--- R/googlespreadsheets.R
+++ R/googlespreadsheets.R
@@ -9,7 +9,7 @@ library(data.table)
 
 #' Loads google spreadsheet via key
 #' @export
-#'
+#' @import googlesheets
 #' @param key key of the google spreadsheet
 #' @param tab name of the tab from which data will be loaded
 #' @param token.file json access token file for Google API.
@@ -20,4 +20,21 @@ gsLoadSheet <- function(key, tab, token.file = "access_token.json") {
   gap <- gs_key(key, verbose = T)
   res <- gs_read(ss = gap, ws = tab)
   res <- data.table(res)
+}
+
+#' Loads all sheets from a google spreadsheet into a list with the tab name as the list element name.
+#' @export
+#' @import googlesheets
+#' @param key Key of the google spreadsheet.
+#' @param token.file Json access token file for Google API.
+gsLoadAll <- function(key, token.file = "access_token.json") {
+  tabs <- gs_key(key)$ws$ws_title
+  sheets <- lapply(tabs,
+                   function(sheet) {
+                     dt <- gsLoadSheet(key = key,
+                                       tab = sheet,
+                                       token.file = token.file)
+                     return(dt)})
+  names(sheets) <- tabs
+  return(sheets)
 }
