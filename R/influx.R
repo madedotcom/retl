9ac0788aed08d2ad97436b883ea29d6920e96fbc
lmavridis <lazaros.mavridis@made.com>
2017-07-17 16:08:09 +0100

    influx in retl

---
+++ R/influx.R
@@ -0,0 +1,51 @@
+#' @import xts
+#' @import influxdbr
+
+library(xts)
+library(influxdbr)
+
+#' Creates db connection based on the environmental variables
+#' @param scheme look at influx_connection
+#' @param host look at influx_connection
+#' @param user look at influx_connection
+#' @param host look at influx_connection
+#' @param pwd look at influx_connection
+#'
+#' @export
+influxGetConnection <- function(scheme = "http",
+                                host = Sys.getenv("INFLUX_HOST"),
+                                user = Sys.getenv("INFLUX_USER"),
+                                port = 8086,
+                                pwd = Sys.getenv("INFLUX_PASSWORD")) {
+
+  # Creates connection to the influxdb in test consul
+  con <- influxdbr::influx_connection(scheme = scheme,
+                                      host = host,
+                                      port = port,
+                                      user = user,
+                                      pass = pwd)
+
+  return(con)
+}
+
+
+#' Logs the result of a job in the influx metric table
+#'@param con a connection object
+#'@param db the database to write
+#'@param job the name of the job to log
+#'@param val the number of records updated
+#'@param metric the metric to be written to
+#'
+#'@export
+etlLogInflux <- function(con, db, job, val, metric){
+  data <- data.table(time = Sys.time(),
+                     job = metric,
+                     value = val)
+
+  xts.data <- as.xts.data.table(data)
+
+  influxdbr::influx_write(con = con,
+                          db = db,
+                          xts = xts.data,
+                          measurement = metric)
+}
