3fb8dd9a723b3d447768902babff74de7e7eab32
Bulat Yapparov <byapparov@gmail.com>
2017-10-18 14:10:40 +0000

    AWS compress functions (#33)
    * Removing file.name param from s3PutFile to match signature of the s3GetFile.
    
    * Added functions for reading and writing as gz and rds on S3.
    
    * Fixing variable names
    
    * Empty line
    
    * Export new functions
    
    * Namespace update
    
    * Typo in read.rds

--- R/aws-s3.R
+++ R/aws-s3.R
@@ -21,19 +21,19 @@ library(aws.s3)
 #' Saves a data.frame structure to AWS S3 as a csv file
 #'
 #' @export
-#' @param df data.frame to save
-#' @param file.name name of the file to be uploaded to S3.
-#' @param path S3 path starting after root folder, excludes filename. example: "folder/"
+#' @param dt data.table to save
+#' @param path S3 object path starting after root folder.
 #' @param bucket name of the S3 bucket
-#' @param root project root path that is appended before the path
-s3PutFile <- function (df, file.name, path="",
+#' @param root project root path that is appended before the path, e.g. "/prod/"
+s3PutFile <- function(dt, path,
                        bucket = Sys.getenv("AWS_S3_BUCKET"),
                        root = Sys.getenv("AWS_S3_ROOT")) {
+  tmp.file <- tempfile(fileext = ".csv")
+  on.exit(unlink(tmp.file))
 
-  write.csv(df, file = file.name, row.names = F)
-  full.path <- paste0(root, path, file.name)
-  put_object(file = file.name, object = full.path, bucket=bucket)
-  if (file.exists(file.name)) file.remove(file.name)
+  write.csv(dt, file = tmp.file, row.names = F, fileEncoding = "UTF-8")
+  full.path <- paste0(root, path)
+  put_object(file = tmp.file, object = full.path, bucket = bucket)
 }
 
 
@@ -48,21 +48,79 @@ s3PutFile <- function (df, file.name, path="",
 s3GetFile <- function(path, header=T,
                       bucket = Sys.getenv("AWS_S3_BUCKET"),
                       root = Sys.getenv("AWS_S3_ROOT")) {
-  # Args:
-  #   path: S3 path starting after root folder, includes filename. example: "folder/file.ext".
   full.path <- paste0(root, path)
   raw_data <- get_object(full.path, bucket)
 
   # In case of error, print error message.
-  if(!is.raw(raw_data))
+  if (!is.raw(raw_data))
     stop(print(raw_data[1:3]))
 
-  data <- iconv(readBin(raw_data, character()), from="UTF-8", to="UTF-8")
-  df <- fread(data, header=header, strip.white = F)
+  data <- iconv(readBin(raw_data, character()), from = "UTF-8", to = "UTF-8")
+  dt <- fread(data, header = header, strip.white = F)
   # Replace " ", "-" and "_" with "." in the header.
-  names(df) <- gsub(" |_|-", ".", tolower(names(df)))
-  return(df)
+  names(dt) <- gsub(" |_|-", ".", tolower(names(dt)))
+  invisible(dt)
 }
+
+#' Saves data.table as csv.gz to S3
+#'
+#' @export
+#' @inherit s3PutFile
+s3PutFile.gz <- function(dt, path,
+                         bucket = Sys.getenv("AWS_S3_BUCKET"),
+                         root = Sys.getenv("AWS_S3_ROOT")) {
+
+  tmp.file <- tempfile(fileext = ".gz")
+  on.exit(unlink(tmp.file))
+  gz.connection <- gzfile(tmp.file, "w")
+  write.csv(dt, file = gz.connection, row.names = F, fileEncoding = "UTF-8")
+  close(gz.connection)
+
+  full.path <- paste0(root, path)
+  put_object(file = tmp.file, object = full.path, bucket = bucket)
+}
+
+#' Loads gz file from s3 and reads it as data.table from csv
+#'
+#' @export
+#' @inherit s3PutFile
+s3GetFile.gz <- function(path,
+                         bucket = Sys.getenv("AWS_S3_BUCKET"),
+                         root = Sys.getenv("AWS_S3_ROOT")) {
+
+  full.path <- paste0(root, path)
+  tmp.file <- tempfile(fileext = ".gz")
+  on.exit(unlink(tmp.file))
+
+  save_object(full.path, bucket, file = tmp.file)
+  dt <- fread(paste0('zcat < ', tmp.file))
+  invisible(dt)
+}
+
+#' Saves file as rds to S3
+#'
+#' @export
+#' @inherit s3PutFile
+s3PutFile.rds <- function(dt, path,
+                          bucket = Sys.getenv("AWS_S3_BUCKET"),
+                          root = Sys.getenv("AWS_S3_ROOT")) {
+
+  full.path <- paste0(root, path)
+  s3saveRDS(dt, bucket = bucket, object = full.path)
+}
+
+#' Saves file as rds to S3
+#'
+#' @export
+#' @inherit s3GetFile
+s3GetFile.rds <- function(path,
+                          bucket = Sys.getenv("AWS_S3_BUCKET"),
+                          root = Sys.getenv("AWS_S3_ROOT")) {
+
+  full.path <- paste0(root, path)
+  s3readRDS(bucket = bucket, object = full.path)
+}
+
 
 #' Gets the path to the object based on the root path setup via
 #' environment variable
