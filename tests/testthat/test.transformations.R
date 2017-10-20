74a3bca4deac7470c47efe6e818e67bfe5d643ed
Bulat Yapparov <byapparov@gmail.com>
2017-10-20 17:48:38 +0100

    Added test for multiple by columns.

--- tests/testthat/test.transformations.R
+++ tests/testthat/test.transformations.R
@@ -21,4 +21,12 @@ test_that("Safe lookup works", {
     dt.lookup <- data.frame(ref = c(1L, 2L, 3L), value = c("a", "b", NA_character_))
     res <- expect_error(safeLookup(dt, dt.lookup, by = "ref"), 'data.frame')
 
+    # can join by two columns
+    dt <- data.table(id = c(1L, 2L, 3L, 4L, 5L), ref = c(2L, 3L, 2L, 4L, 3L), ref.b = c(12L, 13L, 12L, 14L, 14L))
+    dt.lookup <- data.table(ref = c(4L, 1L, 2L, 3L, 3L), ref.b = c(14L, 11L, 12L, 13L, 14L), value = c("c", "a", "b", NA_character_, "x"))
+    res <- safeLookup(dt, dt.lookup, by = c("ref", "ref.b"))
+    expect <- c("b", NA_character_, "b", "c", "x")
+    expect_equal(res$value, expect)
+
+
 })
