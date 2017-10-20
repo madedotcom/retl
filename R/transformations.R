0e1ffeaf769f8592ad38a668bb64db9a24a7010b
Bulat Yapparov <byapparov@gmail.com>
2017-10-20 16:05:16 +0100

    Change order & docs

--- R/transformations.R
+++ R/transformations.R
@@ -1,15 +1,18 @@
-#' Function to safely append values to a data set from another data set via keys
+#' Function to safely append values to a data.table from another data.table via keys
 #'
 #' @export
 #'
-#' @param data data set to which fields will be appended
-#' @param lookup data set from which fields will be appended
+#' @param data data.table to which fields will be appended
+#' @param lookup data.table from which fields will be appended
 #' @param by fields that represent the keys
 #' @param select fields taht will be appended
 #' @return data set with new fields
 #' TODO @Daniel : add unit tests
 safeLookup <- function(data, lookup, by, select = setdiff(colnames(lookup), by)) {
-  # Merges data to lookup making sure that the number of rows does not change. Keeps the original row order.
+
+  if(!is.data.table(data) | !is.data.table(lookup)) {
+    stop("safeLookup does not support data.frames. Convert datasets to data.table")
+  }
 
   if(sum(duplicated(lookup, by = by)) != 0) {
     stop("The 'by' parameter must uniquely link the two data frames.")
@@ -17,10 +20,6 @@ safeLookup <- function(data, lookup, by, select = setdiff(colnames(lookup), by))
 
   if(nrow(data) == 0) {
     stop("Left side data frame must have non-zero number of recrods.")
-  }
-
-  if(!is.data.table(data) | !is.data.table(lookup)) {
-    stop("safeLookup does not support data.frames. Convert datasets to data.table")
   }
 
   tempColName <- paste0(rep(0, max(sapply(colnames(data), nchar)) +1), collapse = "")
