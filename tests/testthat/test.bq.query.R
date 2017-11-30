49be3dc69fb26a03f544a2638d2e7048d35f0e9e
Bulat Yapparov <byapparov@gmail.com>
2017-11-30 10:33:30 +0000

    Fixed the test

--- tests/testthat/test.bq.query.R
+++ tests/testthat/test.bq.query.R
@@ -9,11 +9,11 @@ test_that("Correct sql is executed", {
     {
       res <- bqExecuteSql("SELECT %1$s", "TEST")
       expect <- "SELECT TEST"
-      expect_args(query_function, 1,  sql = expect, project = "", default_dataset = "", max_pages = Inf)
+      expect_args(query_function, 1,  sql = expect, project = "", default_dataset = "", max_pages = Inf, TRUE)
 
       res <- bqExecuteFile("sample_query.sql", 100)
       expect <- "SELECT TEST_100"
-      expect_args(query_function, 2,  sql = expect, project = "", default_dataset = "", max_pages = Inf)
+      expect_args(query_function, 2,  sql = expect, project = "", default_dataset = "", max_pages = Inf, TRUE)
     }
   )
 })
