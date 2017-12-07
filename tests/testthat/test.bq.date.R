cc175b466973fbdd38d9365679dadd4f7a77380d
daniel0000 <daniel.ferenczy@made.com>
2017-12-07 16:17:36 +0000

    Ignore aws.s3 from packrat. Fix tz date issue.

--- tests/testthat/test.bq.date.R
+++ tests/testthat/test.bq.date.R
@@ -1,10 +1,11 @@
 context("BigQuery Dates Tables/Partitions.")
 
 test_that("Missing dates function is correct.", {
+  Sys.setenv(TZ = "GMT")
   start.date <- as.Date("2015-01-10")
   end.date <- as.Date("2015-01-20")
   # Dates are given in format %Y%m%d.
-  existing.dates <-c("20150110", "20150111", "20150112")
+  existing.dates <- c("20150110", "20150111", "20150112")
   res <- getMissingDates(start.date, end.date, existing.dates)
 
   expect_identical(length(res), as.integer(8), "Existing dates are exluded.")
