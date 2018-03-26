b8e84d8cd0ef76d107515bef543c8e891124d2f2
Bulat Yapparov <byapparov@gmail.com>
2018-03-26 13:39:54 +0100

    Explicit function name

--- tests/testthat/test.metadata.R
+++ tests/testthat/test.metadata.R
@@ -9,7 +9,7 @@ test_that("Increment defaults to zero if no records found", {
     `bigrquery::query_exec` = function(sql, project, default_dataset, page_size) {
       res <- data.frame(job = c(), increment_value = c())
     },
-    bqAuth = function() NULL,
+    `retl::bqAuth` = function() NULL,
     {
       res.increment <- etlGetIncrement("test")
       expect_identical(res.increment, as.integer(0))
@@ -24,7 +24,7 @@ test_that("Correct increment is fetched from the database", {
     `bigrquery::query_exec` = function(sql, project, default_dataset, page_size) {
       res <- data.frame(job = c("test"), increment_value = c(100))
     },
-    bqAuth = function() NULL,
+    `retl::bqAuth` = function() NULL,
     {
       res.increment <- etlGetIncrement("test")
       expect_identical(res.increment, 100)
@@ -39,7 +39,7 @@ test_that("Increment is saved to the database correctly", {
       return(values)
     },
     `bigrquery::wait_for` = function(job) job,
-     bqAuth = function() NULL,
+    `retl::bqAuth` = function() NULL,
      logged.data <- etlLogExecution("test", as.integer(10), 100),
      expect_equal(logged.data$increment_value, as.integer(10)),
      expect_equal(logged.data$job, "test")
