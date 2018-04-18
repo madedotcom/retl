80d50f9c11f11495f8ac8ced6518590fab96f543
Bulat Yapparov <byapparov@gmail.com>
2018-04-18 12:06:53 +0100

    Refactored influx logging function to default most values to env vars + rename API.

--- R/influx.R
+++ R/influx.R
@@ -37,25 +37,33 @@ influxConnection <- function(scheme = "http",
 
 
 #' Logs the result of a job in the influx metric table
-#'@param con a connection object
-#'@param db the database to write
+#'@param con a connection object.
+#'  defaults to connection that was set via `influxConnection()` call
+#'@param db the database to write, defaults to value in `INFLUX_DB` env var
 #'@param job the name of the job to log
 #'@param val the variable to log
-#'@param metric the metric to be written to
-#'@param env the environment to write test/prod
+#'@param metric the metric to be written to, defaults to value in `INFLUX_METRIC` env var
+#'@param env the environment to write test/prod, defaults to value in `INFLUX_PROJECT` env var
 #'
 #'@export
-etlLogInflux <- function(con, db, job, val, metric, env){
+influxLog <- function(con = influxConnection(),
+                         db = Sys.getenv("INFLUX_DB"),
+                         job,
+                         val,
+                         metric = Sys.getenv("INFLUX_METRIC"),
+                         env = Sys.getenv("INFLUX_PROJECT")) {
   data <- data.table(time = Sys.time(),
                      value = val)
 
   xts.data <- as.xts.data.table(data)
 
   xts::xtsAttributes(xts.data) <- list(job = job,
-                                  env = env)
+                                       env = env)
 
-  influxdbr::influx_write(con = con,
-                          db = db,
-                          xts = xts.data,
-                          measurement = metric)
+  influxdbr::influx_write(
+    con = con,
+    db = db,
+    xts = xts.data,
+    measurement = metric
+  )
 }
