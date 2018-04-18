6aed343343345fd20d974b22160ecb5ffbb3092b
Bulat Yapparov <byapparov@gmail.com>
2018-04-18 12:15:12 +0100

    Refactor influx connection (#64)
    * Refactor influx connection, so we don't have to maintain global connection variable in projects
    
    * Refactored influx logging function to default most values to env vars + rename API.
    
    * Added news note.

--- R/influx.R
+++ R/influx.R
@@ -4,6 +4,10 @@
 library(influxdbr)
 library(xts)
 
+
+retl_env <- new.env(parent = emptyenv())
+
+
 #' Creates db connection based on the environmental variables
 #' @param scheme look at influx_connection
 #' @param host look at influx_connection
@@ -12,43 +16,54 @@ library(xts)
 #' @param pwd look at influx_connection
 #'
 #' @export
-influxGetConnection <- function(scheme = "http",
+influxConnection <- function(scheme = "http",
                                 host = Sys.getenv("INFLUX_HOST"),
                                 user = Sys.getenv("INFLUX_USER"),
                                 port = 8086,
                                 pwd = Sys.getenv("INFLUX_PASSWORD")) {
 
+  if (!is.null(retl_env$influx_conn)) {
+    return(retl_env$influx_conn)
+  }
   # Creates connection to the influxdb in test consul
-  con <- influxdbr::influx_connection(scheme = scheme,
+  retl_env$influx_conn <- influxdbr::influx_connection(scheme = scheme,
                                       host = host,
                                       port = port,
                                       user = user,
                                       pass = pwd)
+  retl_env$influx_conn
 
-  return(con)
 }
 
 
 #' Logs the result of a job in the influx metric table
-#'@param con a connection object
-#'@param db the database to write
+#'@param con a connection object.
+#'  defaults to connection that was set via `influxConnection()` call
+#'@param db the database to write, defaults to value in `INFLUX_DB` env var
 #'@param job the name of the job to log
 #'@param val the variable to log
-#'@param metric the metric to be written to
-#'@param env the environment to write test/prod
+#'@param metric the metric to be written to, defaults to value in `INFLUX_METRIC` env var
+#'@param env the environment to write test/prod, defaults to value in `INFLUX_PROJECT` env var
 #'
 #'@export
-etlLogInflux <- function(con, db, job, val, metric, env){
+influxLog <- function(con = influxConnection(),
+                         db = Sys.getenv("INFLUX_DB"),
+                         job,
+                         val,
+                         metric = Sys.getenv("INFLUX_METRIC"),
+                         env = Sys.getenv("INFLUX_PROJECT")) {
   data <- data.table(time = Sys.time(),
                      value = val)
 
   xts.data <- as.xts.data.table(data)
 
   xts::xtsAttributes(xts.data) <- list(job = job,
-                                  env = env)
+                                       env = env)
 
-  influxdbr::influx_write(con = con,
-                          db = db,
-                          xts = xts.data,
-                          measurement = metric)
+  influxdbr::influx_write(
+    con = con,
+    db = db,
+    xts = xts.data,
+    measurement = metric
+  )
 }
---
+++ news.md
@@ -0,0 +1,3 @@
+# retl 0.1.0
+
+`influxLog()`, `influxConnection()` changed the API of the influx wrapper functions to default values. (#64, byapparov)
