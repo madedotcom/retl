ec133ba64bad5c96a30d6ccd80267358968eb96c
daniel0000 <daniel.ferenczy@made.com>
2018-06-06 12:11:49 +0100

    Add documentation.

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,4 +5,8 @@ suppressPackageStartupMessages({
   library(retl)
 })
 
+# .Renviron:
+# BIGQUERY_TEST_PROJECT
+# BIGQUERY_ACCESS_TOKEN_PATH
+# The access token file needs to be in the testthat folder
 test_check("retl")
