5e070233793166810ff423abe35ff18ad64fe83a
byapparov <byapparov@gmail.com>
2018-06-18 21:10:28 +0100

    Added aws.s3 tests

--- tests/testthat/test.aws.s3.R
+++ tests/testthat/test.aws.s3.R
@@ -7,3 +7,50 @@ test_that("Path is calculated correctly", {
   expected <- paste0("prod/project/", relative.path)
   expect_identical(res, expected)
 })
+
+test_that("function is matching the path name", {
+  path <- "test/file_name.csv"
+  res <- s3GetFile.factory(path)
+  expect_equal(res, s3GetFile.csv)
+
+  path <- "test/file_name.json.gz"
+  res <- s3GetFile.factory(path)
+  expect_equal(res, s3GetFile.json.gz)
+
+  path <- "test/file_name.zip"
+  res <- s3GetFile.factory(path)
+  expect_equal(res, s3GetFile.zip)
+
+  path <- "test/file_name.csv.gz"
+  res <- s3GetFile.factory(path)
+  expect_equal(res, s3GetFile.gz)
+
+  path <- "test/file_name.gz"
+  res <- s3GetFile.factory(path)
+  expect_equal(res, s3GetFile.gz)
+
+  path <- "test/file_name.bad"
+  res <-
+  expect_error(s3GetFile.factory(path), regexp = "extension.*bad")
+
+  # Put File
+  path <- "test/file_name.csv"
+  res <- s3PutFile.factory(path)
+  expect_equal(res, s3PutFile.csv)
+
+  path <- "test/file_name.json.gz"
+  res <- s3PutFile.factory(path)
+  expect_equal(res, s3PutFile.json.gz)
+
+  path <- "test/file_name.bad"
+  expect_error(s3PutFile.factory(path), regexp = "extension.*bad")
+
+  path <- "test/file_name.csv.gz"
+  res <- s3PutFile.factory(path)
+  expect_equal(res, s3PutFile.gz)
+
+  path <- "test/file_name.gz"
+  res <- s3PutFile.factory(path)
+  expect_equal(res, s3PutFile.gz)
+
+})
