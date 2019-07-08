4311afa439c3685e4babc785e89e8f2605157e60
byapparov <byapparov@gmail.com>
2019-07-08 22:59:11 +0000

    Auth once

--- tests/testthat/helper-env.R
+++ tests/testthat/helper-env.R
@@ -2,10 +2,7 @@ if (gargle:::secret_can_decrypt("retl")) {
   print("Can decrypt")
   json <- gargle:::secret_read(package = "retl", name = "retl-testing.json")
   bigrquery::bq_auth(path = rawToChar(json))
-}
 
-if (bqTokenFileValid()) {
-  bqAuth()
   ds <- bigrquery::bq_test_dataset()
   Sys.setenv(BIGQUERY_DATASET = ds$dataset)
   Sys.setenv(BIGQUERY_PROJECT = Sys.getenv("BIGQUERY_TEST_PROJECT"))
