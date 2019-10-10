278fd9a55e2580412dd80b1e0641ddbb002099d3
byapparov <byapparov@gmail.com>
2019-10-10 23:15:52 +0000

    Don't save the file locally

--- tests/testthat/helper-env.R
+++ tests/testthat/helper-env.R
@@ -4,10 +4,6 @@ if (gargle:::secret_can_decrypt("retl")) {
   bigrquery::bq_auth(path = rawToChar(json))
   googleCloudStorageR::gcs_auth(json_file = rawToChar(json))
 
-  tmp <- tempfile(fileext = ".json")
-  write(rawToChar(json), file = tmp)
-  Sys.setenv("GCS_AUTH_FILE" = tmp)
-
   ds <- bigrquery::bq_test_dataset()
   Sys.setenv(BIGQUERY_DATASET = ds$dataset)
   Sys.setenv(BIGQUERY_PROJECT = Sys.getenv("BIGQUERY_TEST_PROJECT"))
