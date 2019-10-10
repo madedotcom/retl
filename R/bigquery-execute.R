48820516e5769d87e7802a793d2a550e782e5df5
Bulat Yapparov <byapparov@gmail.com>
2019-10-10 17:44:47 +0100

    added namespace

--- R/bigquery-execute.R
+++ R/bigquery-execute.R
@@ -137,7 +137,7 @@ bqDownloadQuery <- function(query, ...) {
   export.format <- "CSV"
   export.compression <- "GZIP"
 
-  gcs_global_bucket(Sys.getenv("GCS_DEFAULT_BUCKET"))
+  googleCloudStorageR::gcs_global_bucket(Sys.getenv("GCS_DEFAULT_BUCKET"))
 
   # Execute Query to get results into a temporary table
   job <- bqExecuteDml(query, ...)
