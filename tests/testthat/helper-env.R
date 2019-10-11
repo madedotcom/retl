15fa09f420b7a591145073510cf5b1c31ebf8094
Bulat Yapparov <byapparov@gmail.com>
2019-10-11 09:23:33 +0000

    Fix the tests

--- tests/testthat/helper-env.R
+++ tests/testthat/helper-env.R
@@ -2,6 +2,7 @@ if (gargle:::secret_can_decrypt("retl")) {
   print("Can decrypt retl")
   json <- gargle:::secret_read(package = "retl", name = "retl-testing.json")
   bigrquery::bq_auth(path = rawToChar(json))
+  googleCloudStorageR::gcs_auth(json_file = rawToChar(json))
 
   ds <- bigrquery::bq_test_dataset()
   Sys.setenv(BIGQUERY_DATASET = ds$dataset)
