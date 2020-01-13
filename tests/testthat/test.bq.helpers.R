aa08652aee59487ef850aebba44154fa7ec7dea2
Bulat Yapparov <byapparov@gmail.com>
2020-01-13 14:39:31 +0000

    Fixed tests + updated doc

--- tests/testthat/test.bq.helpers.R
+++ tests/testthat/test.bq.helpers.R
@@ -1,6 +1,6 @@
 context("BigQuery Helpers")
 
-test_that("bqCountDuplicates function is correct", {
+test_that("bqCountDuplicates function counts duplicate appearances in primary key", {
   test_data = data.frame(
     id = c(1L, 2L, 3L, 4L),
     order_reference = c("101", "101", "202", "202"),
@@ -8,16 +8,14 @@ test_that("bqCountDuplicates function is correct", {
   )
 
   bqInsertData(
-    "bq_count_duplicates",
-    test_data,
-    "test",
+    table = "bq_count_duplicates",
+    data = test_data,
     append = FALSE
   )
 
   # Zero duplicate rows when grouped by "id"
   res <- bqCountDuplicates(
     table = "bq_count_duplicates",
-    dataset = "test",
     keys = "id"
   )
   expect_identical(res, 0L)
@@ -25,7 +23,6 @@ test_that("bqCountDuplicates function is correct", {
   # 1 duplicate row when grouped by "order_reference", "sku"
   res <- bqCountDuplicates(
     table = "bq_count_duplicates",
-    dataset = "test",
     keys = c(
       "order_reference",
       "sku"
@@ -33,18 +30,19 @@ test_that("bqCountDuplicates function is correct", {
   )
   expect_identical(res, 1L)
 
+})
+
+test_that("bqCountDuplicates returns zero for empty tables", {
+
   # Test that function works for empty table
   bqInitiateTable(
     table ="bq_empty_table",
-    dataset = "test",
     schema.file = "bq-table-schema.json"
   )
 
   res <- bqCountDuplicates(
     table = "bq_empty_table",
-    dataset = "test",
     keys = "id"
   )
   expect_identical(res, 0L)
-
 })
