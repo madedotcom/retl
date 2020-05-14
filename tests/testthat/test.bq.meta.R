23da4daaa9b77e7888d8e1c1dbccebd0d8822efc
Bulat Yapparov <byapparov@gmail.com>
2020-05-15 00:36:19 +0100

    Added functions to update table options.

---
+++ tests/testthat/test.bq.meta.R
@@ -0,0 +1,17 @@
+context("BigQuery Meta")
+
+load
+test_that("Table description is updated", {
+  tbl <- bqInitiateTable(
+    table = "test_description_update",
+    schema.file = "bq-table-schema.json"
+  )
+  bqUpdateTableDescription(
+    table = "test_description_update",
+    description = "test"
+  )
+
+  meta <- bigrquery::bq_table_meta(tbl)
+  expect_equal(meta$description, "test")
+  bqDeleteTable("test_description_update")
+})
